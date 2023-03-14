62939b4cf6baea9ee5a4813bb4db04b43ae22273
Elsa Katrín <elsa.katrin@gmail.com>
2023-03-14 13:43:50 +0000

    started on onboarding and added icons

---
+++ betterstep/src/components/Onboarding/Onboarding.jsx
@@ -0,0 +1,12 @@
+import styles from './Onboard.module.css'
+import Link from 'next/link'
+
+export default function Onboarding() { 
+    return(
+        <>
+        <div className={styles.container}>
+            
+        </div>
+        </>
+    )
+}
