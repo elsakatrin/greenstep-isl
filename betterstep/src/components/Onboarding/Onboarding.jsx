f86ead0404cb54a1be37bba344840ac8a4a0e32c
Elsa Katrín <elsa.katrin@gmail.com>
2023-03-14 14:01:52 +0000

    started on onboarding and added icons inside public

--- betterstep/src/components/Onboarding/Onboarding.jsx
+++ betterstep/src/components/Onboarding/Onboarding.jsx
@@ -1,11 +1,18 @@
 import styles from './Onboard.module.css'
 import Link from 'next/link'
 
-export default function Onboarding() { 
+//On boarding for the first time you log in. 
+//This is a reusable component so the contents inside change
+//Children prop lets us put html inside when we import the component inside pages
+export default function Onboarding({title, children}) { 
     return(
         <>
+        <Head>
+            <title>{title}</title>
+        </Head>
         <div className={styles.container}>
-            
+            <h1>{heading}</h1>
+            {children}
         </div>
         </>
     )
