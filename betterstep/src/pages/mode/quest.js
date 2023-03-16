e79927e721a46069fd326b0f73cd6ab4b68fa702
Birkeholm <mikkel.birkeholm@gmail.com>
2023-03-16 20:48:30 +0000

    Retry

--- betterstep/src/pages/mode/quest.js
+++ betterstep/src/pages/mode/quest.js
@@ -3,21 +3,19 @@ import Layout from "@/components/Layout/Layout";
 import Map from "@/components/Map/Map";
 import Link from "next/link";
 import Button from "@/components/Button/Button";
-import ComingSoon from "@/components/ComingSoon/ComingSoon";
-// import styles from "../../components/Comingsoon/ComingSoon.module.css"
+
 
 export default function Quest() {
     return (
         <>
             <Map />
-            <ComingSoon heading="Coming soon!">
-                <div className={styles.content}>
-                    <p className={styles.comingsoon}>Questmode is coming soon, stay tuned!</p>
-                    <Link className={styles.gobtn} href="/mode/explore">
-                        <Button >Go explore!</Button>
-                    </Link>
-                </div>
-            </ComingSoon>
         </>
     )
 }
+
+// </div>
+            // <div className={styles.content}>
+            //     <p className={styles.comingsoon}>Questmode is coming soon, stay tuned!</p>
+            //     <Link className={styles.gobtn} href="/mode/explore">
+            //         <Button >Go explore!</Button>
+            //     </Link>
